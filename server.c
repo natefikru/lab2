e58a7df6a0d1ead1199bca7826c4e4c21af17527
Kevin Mueller <kevinmueller@Kevins-MacBook-Pro-2.local>
2015-11-09 20:01:29 -0600

    more messages

--- server.c
+++ server.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-
+#include <string>
 #define MAX_BUF 1024
 
 int main()
@@ -19,7 +19,24 @@ int main()
 		if(strcmp(buf, "hi") == 0){
 
 			printf("Received: %s\n", buf);
+			printf("Returned: Hello!");
 			close(fd);
+		}
+
+		else if(strcmp(buf, "nate") == 0){
+			printf("Received: %s\n", buf);
+			printf("Returned: fikru");
+			close(fd);
+		}
+
+		else if(strcmp(buf, "kevin") == 0){
+			printf("Received: %s\n", buf);
+			printf("Returned: mueller");
+			close(fd);
+		}
+		else{
+		   printf("Received: %s\n", buf);
+		   printf("Returned: Random Message");
 		}
  //   }
 	return 0;
