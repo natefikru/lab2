0e5ba2c25766d533c4f3f4c0b3276007d05d2948
Kevin Mueller <kevinmueller@Kevins-MacBook-Pro-2.local>
2015-11-10 10:22:28 -0600

    sends pid

--- client.c
+++ client.c
@@ -3,21 +3,48 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+
+
+
 
 int main()
 {
 
+
+	pid_t pid;
+	pid = getpid();
+
+
+	char pid_string[10];
 	char input[20];
+	char send_str[30];
+	char tmpbuf[12];
+	sprintf(tmpbuf, "%d", pid);
+	strcat(pid_string,tmpbuf);
+
+
 	printf("enter your input: ");
 	scanf("%s", input);
-	printf("this is your input: %s\n", input);
+	char sendString[50];
+	sendString[30] = input;
+	strcat(send_str, pid_string);
+	printf("%s\n", pid_string);
+	strcat(send_str, input);
+	printf("this is your input: %s\n", send_str);
 	int fd;
 
+
+	printf("SIZE DATA %d\n", sizeof(send_str));
+	
 	char * myfifo = "/tmp/myfifo";
 	mkfifo(myfifo, 0666);
+	printf("work");
 	fd = open(myfifo, O_WRONLY);
 
-	write(fd, input, sizeof(input));
+	
+	write(fd, send_str, sizeof(send_str));
+	printf("test");
 	close(fd);
 	unlink(myfifo);
 
--- server.c
+++ server.c
@@ -2,42 +2,47 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <string>
 #define MAX_BUF 1024
+#include <string.h>
+typedef struct{
+	int pid;
+	char *name;
+}nameData;
 
 int main()
 {
 	int fd;
 	char * myfifo = "/tmp/myfifo";
 	char buf[MAX_BUF];
-
+	nameData data;
 	fd = open(myfifo, O_RDONLY);
 	read(fd, buf, MAX_BUF);
+	int pid;
+	int n = sscanf(buf, "%d", &pid);
+	printf("pid %d\n", pid);:w
 
-//	while(buf != "exit"){
+//		if(strcmp(buf, "hi") == 0){
 
-		if(strcmp(buf, "hi") == 0){
-
-			printf("Received: %s\n", buf);
-			printf("Returned: Hello!");
-			close(fd);
-		}
-
-		else if(strcmp(buf, "nate") == 0){
-			printf("Received: %s\n", buf);
-			printf("Returned: fikru");
-			close(fd);
-		}
-
-		else if(strcmp(buf, "kevin") == 0){
-			printf("Received: %s\n", buf);
-			printf("Returned: mueller");
-			close(fd);
-		}
-		else{
-		   printf("Received: %s\n", buf);
-		   printf("Returned: Random Message");
-		}
+//			printf("Received: %s\n", buf);
+//			printf("Returned: Hello!");
+//			close(fd);
+//		}
+//
+//		else if(strcmp(buf, "nate") == 0){
+//			printf("Received: %s\n", buf);
+//			printf("Returned: fikru");
+//			close(fd);
+//		}
+//
+//		else if(strcmp(buf, "kevin") == 0){
+//			printf("Received: %s\n", buf);
+//			printf("Returned: mueller");
+//			close(fd);
+//		}
+//		else{
+//		   printf("Received: %s\n", buf);
+//		   printf("Returned: Random Message");
+//		}
  //   }
 	return 0;
 }
