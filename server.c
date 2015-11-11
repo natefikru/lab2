37418321b181091128079fd1063d82da9b2d1601
nate fikru <natefikru@gmail.com>
2015-11-10 19:08:22 -0600

    added semaphore bullish

--- server.c
+++ server.c
@@ -2,47 +2,58 @@
 #include <stdio.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#define MAX_BUF 1024
 #include <string.h>
-typedef struct{
-	int pid;
-	char *name;
-}nameData;
+#include <pthread.h>
+#include <semaphore.h>
 
-int main()
+#define MAX_BUF 1024
+
+void serverListener(void *ptr);
+
+char buf[24];
+
+sem_t mutex;
+
+int main(int argc, char *argv[])
 {
+	pthread_t thread0;
+	pthread_t thread1;
+	pthread_t thread2;
+
+	sem_init (&mutex, 0, 1);
+
+	char *msg1 = "";
+
+	pthread_create (&thread0, NULL, (void *) &serverListener, (void *) msg1);
+	pthread_create (&thread1, NULL, (void *) &serverListener, (void *) msg1);
+	pthread_create (&thread2, NULL, (void *) &serverListener, (void *) msg1);
+
+	pthread_join (thread0, NULL);
+	pthread_join (thread0, NULL);
+	pthread_join (thread0, NULL);
+
+	sem_destroy (&mutex);
+
+	return 0;
+}
+
+void serverListener(void *ptr){
+
 	int fd;
 	char * myfifo = "/tmp/myfifo";
 	char buf[MAX_BUF];
-	nameData data;
+
 	fd = open(myfifo, O_RDONLY);
 	read(fd, buf, MAX_BUF);
+
+	//put mutex around the critical area
+	sem_wait(&mutex);
+	//then update buffer
+
 	int pid;
 	int n = sscanf(buf, "%d", &pid);
-	printf("pid %d\n", pid);:w
+	printf("pid %d\n", pid);
 
-//		if(strcmp(buf, "hi") == 0){
-
-//			printf("Received: %s\n", buf);
-//			printf("Returned: Hello!");
-//			close(fd);
-//		}
-//
-//		else if(strcmp(buf, "nate") == 0){
-//			printf("Received: %s\n", buf);
-//			printf("Returned: fikru");
-//			close(fd);
-//		}
-//
-//		else if(strcmp(buf, "kevin") == 0){
-//			printf("Received: %s\n", buf);
-//			printf("Returned: mueller");
-//			close(fd);
-//		}
-//		else{
-//		   printf("Received: %s\n", buf);
-//		   printf("Returned: Random Message");
-//		}
- //   }
-	return 0;
+	//end the mutex
+	sem_post(&mutex);
 }
