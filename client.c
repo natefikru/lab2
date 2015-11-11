5dee0369dc9d5d1a947c8d61bbfc04ea2073fc07
Kevin Mueller <kevinmueller@Kevins-MacBook-Pro-2.local>
2015-11-11 16:57:31 -0600

    not fucking working right

--- client.c
+++ client.c
@@ -10,52 +10,63 @@
 
 int main()
 {
-
-
 	pid_t pid;
 	pid = getpid();
-
 
 	char pid_string[10];
 	char input[20];
 	char send_str[30];
 	char tmpbuf[12];
 
-	
+	//parse pid int to string
 	sprintf(tmpbuf, "%d", pid);
 	strcat(pid_string,tmpbuf);
 
+	printf("\nCELEBRITY LAST NAME MATCHER\n");
+	printf("\nTom\n");
+	printf("Bob\n");
+	printf("Mark\n");
+	printf("Matt\n");
+	printf("Ben\n");
+	printf("Selena\n");
+	printf("Kate\n");
+	printf("Hillary\n\n");
+
+	printf("PLEASE INPUT A CELEBRITY NAME THAT IS LISTED ABOVE.\n");
+	printf("YOU WILL RECEIVE THEIR LAST NAME\n\n");
+	printf("NAME MUST BE INPUTTED EXACTLY AS ABOVE!!\n");
 
 
-
-	printf("enter your input: ");
+	//allow for user input
+	printf("Enter your input: ");
 	scanf("%s", input);
+	
+//	concatanate send string with pid at front and msg following
 	char sendString[50];
 	sendString[30] = input;
 	strcat(send_str, pid_string);
-	printf("PID STRING: %s\n", pid_string);
 	strcat(send_str, input);
-	printf("this is your input: %s\n", send_str);
-	int fd;
-
-
-	printf("SIZE DATA %d\n", sizeof(send_str));
+	printf("First Name: %s\n", input);
 	
+	//create temp fifo for server to open
+	int fd;
 	char * myfifo = "/tmp/myfifo";
 	mkfifo(myfifo, 0666);
 	fd = open(myfifo, O_WRONLY);
 
-	
+	//write send str to fifo,close
 	write(fd, send_str, sizeof(send_str));
 	close(fd);
 	unlink(myfifo);
 
-	//open fifo pipe from server
+	//wait for server to create response fifo
+	sleep(2);
+	
+	//find fifo pipe from server
 	char tmpPath[10] = "/tmp/";
 	char returnFifo[40];
 	strcat(returnFifo, tmpPath);
 	strcat(returnFifo, pid_string);
-	printf("return path is %s|\n", returnFifo);
 	
 
 	//open return fifo pipe
@@ -64,11 +75,7 @@ int main()
 	returnPipe = open(returnFifo, O_RDONLY);
 	read(returnPipe, returnBuf, MAX_BUF);
 	char * msg = returnBuf;
-	printf("return size of buffer %d\n", sizeof(returnBuf));	
-	printf("char 1 %c", returnBuf[1]);
+	printf("buffer contents %s\n",returnBuf);	
 
-
-	
-	
 	return 0;
 }
--- server.c
+++ server.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	pthread_t thread1;
 	pthread_t thread2;
 
-	sem_init (&mutex, 0, 1);
+	sem_init(&mutex, 0, 1);
 
 	char *msg1 = "";
 
@@ -31,10 +31,10 @@ int main(int argc, char *argv[])
 	pthread_create (&thread2, NULL, (void *) &serverListener, (void *) msg1);
 
 	pthread_join (thread0, NULL);
-	pthread_join (thread0, NULL);
-	pthread_join (thread0, NULL);
+	pthread_join (thread1, NULL);
+	pthread_join (thread2, NULL);
 
-	sem_destroy (&mutex);
+	sem_destroy(&mutex);
 
 	return 0;
 }
@@ -43,13 +43,17 @@ void serverListener(void *ptr){
   while(1){
 	sem_wait(&mutex);		
 
-
 	int fd;
 	char * myfifo = "/tmp/myfifo";
 	char buf[MAX_BUF];
 
 	fd = open(myfifo, O_RDONLY);
+	printf("FD STATUS %d\n", fd);
+	
 	read(fd, buf, MAX_BUF);
+	printf("buf conents %s\n", buf);
+	
+	
 
 	//put mutex around the critical area
 	//then update buffer
@@ -63,35 +67,64 @@ void serverListener(void *ptr){
 	printf("PID LENGTH %d\n", numDigitsPid);
 	printf("pid %d\n", pid);
 
+//	sem_post(&mutex);	
 	
+	char *firstName = &buf[numDigitsPid];
 	
-	char *as = &buf[numDigitsPid];
-	printf("%s", as);	
+		
+	char * lastName;
+	printf("FIRST NAME IS %s\n", firstName);
+	if(strcmp(firstName,"Tom") == 0){
+	   lastName = "Cruise";
+	}
+	else if(strcmp(firstName, "Bob") == 0){
+	    lastName = "Dylan";
+	}
+	else if(strcmp(firstName, "Mark") == 0){
+	   lastName = "Langanki";
+	}
+ 	else if(strcmp(firstName, "Matt") == 0){
+  	   lastName = "Damon";
+        }
+	else if(strcmp(firstName, "Ben") == 0){
+	   lastName = "Affleck";
+	}
+	else if(strcmp(firstName, "Selena") == 0){
+          lastName = "Gomez";
+	}
+	else if(strcmp(firstName, "Kate") == 0){
+	  lastName = "Winslet";
+	}
+	else if(strcmp(firstName, "Hillary") == 0){
+          lastName = "Clinton";
+	}
+
+	printf("Requested Last Name is: %s\n", lastName);
 
 	//make the string with correct path for client fifo pipe
-	
-	//parse pid int to str
+
+
+	//parse pid int to str, concatanate proper string for path to client fifo
 	char pidString[10];
 	sprintf(pidString, "%d", pid);
 	char * tmpPath = "/tmp/";
-	char clientPath[20];
+	char clientPath[50];
 	strcat(clientPath, tmpPath);
 	strcat(clientPath,pidString);
-	printf("client path is %s\n", clientPath);
 	
 	//create fifo pipe for client
 	char clientBuf[MAX_BUF];
-	
-	mkfifo(clientPath, 0777);
+	mkfifo(clientPath, 0666);
 	int clientPipe = open(clientPath, O_WRONLY);
-	write(clientPipe, clientPath, sizeof(clientPath));
+	write(clientPipe, lastName, sizeof(lastName));
 	close(clientPipe);
 	unlink(clientPath);
-
-
-	printf("%s\n", msg);
+	
 	//end the mutex
 	sem_post(&mutex);
-   }
+	
+	pthread_exit(0);
+
+  }
 
 }
